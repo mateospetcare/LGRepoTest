38a319b879c2c285f80d819f4c5a1a5ca2a67466
Baibhav Gautam <baibhavx@gmail.com>
2017-04-17 20:43:57 -0700

    Add more questions

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//Function reverses the input string
 function reverse(string) {
   //todo
+}
+
+//Function shuffles the input string
+function shuffle(string) {
+//todo
+}
+
+//swaps a->z, b->y and so on.
+function swapLetters(string) {
+//todo
 }
