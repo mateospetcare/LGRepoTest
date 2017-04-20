2cb52d431de4ff1cba998f19cc22d447a852b8b9
Robert Nielsen <coderhyder@gmail.com>
2017-04-20 03:56:37 +0000

    Mo' Better.
    Let me know if it works with the code

--- stringreverse.js
+++ stringreverse.js
@@ -1,7 +1,6 @@
 // function needs html header to be utf-16....
 function robert(string){ //reverse the string
-	return s.split("").reverse().join("");
-	}
-	return "Cannot reverse the string given";
+	return string.split("").reverse().join("");
 }
+	
 
